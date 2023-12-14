9c505709626f55933ba67a3f93441eb739950108
carvalDev <carvalhohugo425@gmail.com>
2023-12-14 09:48:52 -0300

    Atualiza

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] teste = {1,2,3};
-        int[] teste1 = teste;
-        System.out.println(teste1[0]);
+        System.out.println("Hello World!");
     }
 }
